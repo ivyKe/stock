a48593d57028552f32954d9b62b2b7686abc53a0
ivyke <ivy4301@gmail.com>
2017-04-21 11:35:26 +0800

    Add Action type and Action

---
+++ app/actions/actions-test.js
@@ -0,0 +1,47 @@
+import * as actions from './index';
+import * as actionTypes from '../constants/actionTypes';
+import { expect } from 'chai';
+
+describe('Actions Test', () => {
+//  it('initStockInfo', () => {
+//    expect(actions.initStockInfo()).to.deep.equal({
+//      type: actionTypes.INIT_STOCK_INFO
+//    });
+//  });
+
+  it('addStock', () => {
+    expect(actions.addStock('stock:name')).to.deep.equal({
+      type: actionTypes.ADD_STOCK, name: 'stock:name',
+    });
+  });
+
+  it('selectStock', () => {
+    expect(actions.selectStock('stock:name')).to.deep.equal({
+      type: actionTypes.SELECT_STOCK, name: 'stock:name',
+    });
+  });
+
+  it('removeStock', () => {
+    expect(actions.removeStock('stock:name')).to.deep.equal({
+      type: actionTypes.REMOVE_STOCK, name: 'stock:name',
+    });
+  });
+
+  it('refreshStockInfo', () => {
+    const info = {
+        name: 'stock:name', value: 10, change: 3.5,
+    }
+    expect(actions.refreshStockInfo(info)).to.deep.equal({
+      type: actionTypes.REFRESH_STOCK_INFO, info
+    });
+  });
+
+  it('refreshStockHistory', () => {
+    const history = {
+        name: 'stock:name', history: []
+    }
+    expect(actions.refreshStockHistory(history)).to.deep.equal({
+      type: actionTypes.REFRESH_STOCK_HISTORY, history,
+    });
+  });
+});
--- app/actions/index.js
+++ app/actions/index.js
@@ -1,1 +1,36 @@
-// ..
+import * as actionTypes from '../constants/actionTypes';
+
+/**
+ ** 新增股票
+ */
+export function addStock(name){
+    return {type:actionTypes.ADD_STOCK,name};
+}
+
+/**
+ ** 刪除股票
+ */
+export function removeStock(name){
+    return {type:actionTypes.REMOVE_STOCK,name};
+}
+
+/**
+ ** 選擇股票
+ */
+export function selectStock(name){
+    return {type:actionTypes.SELECT_STOCK,name};
+}
+
+/**
+ ** 更新股票資訊
+ */
+export function refreshStockInfo(info){
+    return {type:actionTypes.REFRESH_STOCK_INFO,info};
+}
+
+/**
+ ** 更新股票歷史資料
+ */
+export function refreshStockHistory(history){
+    return {type:actionTypes.REFRESH_STOCK_HISTORY,history};
+}
